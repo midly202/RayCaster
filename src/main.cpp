28e89edfa8cd69679247c0a88458909474bd040d
Midly <svangasteren05@gmail.com>
2025-06-22 11:37:13 +0200

    Fixed inconsistent type annotation in WinMain to shut up MSVC
    Added build dir to .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@
 
 #ifdef _WIN32
 #include <windows.h>
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+int WINAPI WinMain(
+    _In_     HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_     LPSTR     lpCmdLine,
+    _In_     int       nCmdShow
+)
 #else
 int main()
 #endif
